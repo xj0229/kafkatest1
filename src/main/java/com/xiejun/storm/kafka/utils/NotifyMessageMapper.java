f1f999393ab18a21f33217292255c35d59d6966b
xiejun <xiejun@qq.com>
2016-12-27 19:29:03 +0800

    add new class NotifyMessageMapper

---
+++ src/main/java/com/xiejun/storm/kafka/utils/NotifyMessageMapper.java
@@ -0,0 +1,13 @@
+package com.xiejun.storm.kafka.utils;
+
+import org.apache.storm.trident.tuple.TridentTuple;
+
+public class NotifyMessageMapper implements MessageMapper{
+
+	@Override
+	public String toMessageBody(TridentTuple tuple) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
