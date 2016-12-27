26d5ccf8ff8ca4d10af8d97fdb5182aa1c03e791
xiejun <xiejun@qq.com>
2016-12-27 20:04:57 +0800

    first complete

--- src/main/java/com/xiejun/storm/kafka/topology/LogAnalysisTopology.java
+++ src/main/java/com/xiejun/storm/kafka/topology/LogAnalysisTopology.java
@@ -1,5 +1,11 @@
 package com.xiejun.storm.kafka.topology;
 
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.StormSubmitter;
+import org.apache.storm.generated.AlreadyAliveException;
+import org.apache.storm.generated.AuthorizationException;
+import org.apache.storm.generated.InvalidTopologyException;
 import org.apache.storm.generated.StormTopology;
 import org.apache.storm.kafka.BrokerHosts;
 import org.apache.storm.kafka.StringScheme;
@@ -10,6 +16,16 @@ import org.apache.storm.spout.SchemeAsMultiScheme;
 import org.apache.storm.topology.IRichSpout;
 import org.apache.storm.trident.Stream;
 import org.apache.storm.trident.TridentTopology;
+import org.apache.storm.tuple.Fields;
+
+import com.xiejun.storm.kafka.filter.BooleanFilter;
+import com.xiejun.storm.kafka.function.JsonProjectFunction;
+import com.xiejun.storm.kafka.function.MovingAverageFunction;
+import com.xiejun.storm.kafka.function.ThresholdFilterFunction;
+import com.xiejun.storm.kafka.function.XMPPFunction;
+import com.xiejun.storm.kafka.utils.EWMA;
+import com.xiejun.storm.kafka.utils.EWMA.Time;
+import com.xiejun.storm.kafka.utils.NotifyMessageMapper;
 
 public class LogAnalysisTopology {
 	
@@ -22,13 +38,54 @@ public class LogAnalysisTopology {
 		
 		spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
 		
-		OpaqueTridentKafkaSpout spout = new OpaqueTridentKafkaSpout(spoutConf);
-		
 		spoutConf.startOffsetTime = -1;
 		
-		Stream spoutStream = topology.newStream("kafka-stream", (IRichSpout) spoutConf);
+		OpaqueTridentKafkaSpout spout = new OpaqueTridentKafkaSpout(spoutConf);
 		
+		Stream spoutStream = topology.newStream("kafka-stream", spout);
 		
+		Fields jsonFields = new Fields("level", "timestamp", "message", "logger");
+		
+		Stream parsedStream = spoutStream.each(new Fields("str"), new JsonProjectFunction(jsonFields), jsonFields);
+		
+		parsedStream = parsedStream.project(jsonFields);
+		
+		EWMA ewma = new EWMA().sliding(1.0, Time.MINUTES).withAlpha(EWMA.ONE_MINUTE_ALPHA);
+		
+		Stream averageStream = parsedStream.each(new Fields("timestamp"), new MovingAverageFunction(ewma, Time.MINUTES), new Fields("average"));
+		
+		ThresholdFilterFunction tff = new ThresholdFilterFunction(50D);
+		
+		Stream thresholdStream = averageStream.each(new Fields("average"), tff, new Fields("change", "threshold"));
+		
+		Stream filteredStream = thresholdStream.each(new Fields("change"), new BooleanFilter());
+		
+		filteredStream.each(filteredStream.getOutputFields(), new XMPPFunction(new NotifyMessageMapper()), new Fields());
+		
+		return topology.build();
+		
+	}
+	
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException, AuthorizationException{
+		Config config = new Config();
+		
+		config.put(XMPPFunction.XMPP_USER, "storm@budreau.local");
+		
+		config.put(XMPPFunction.XMPP_PASSWORD, "storm");
+		
+		config.put(XMPPFunction.XMPP_SERVER, "budreau.local");
+		
+		config.put(XMPPFunction.XMPP_TO, "tgoetz@budreau.local");
+		
+		config.setMaxSpoutPending(5);
+		
+		if(args.length == 0){
+			LocalCluster cluster = new LocalCluster();
+			cluster.submitTopology("log-analysis", config, buildTopology());
+		}else{
+			config.setNumWorkers(3);
+			StormSubmitter.submitTopology(args[0], config, buildTopology());
+		}
 		
 	}
 	
--- src/main/java/com/xiejun/storm/kafka/utils/NotifyMessageMapper.java
+++ src/main/java/com/xiejun/storm/kafka/utils/NotifyMessageMapper.java
@@ -1,5 +1,7 @@
 package com.xiejun.storm.kafka.utils;
 
+import java.util.Date;
+
 import org.apache.storm.trident.tuple.TridentTuple;
 
 public class NotifyMessageMapper implements MessageMapper{
@@ -7,7 +9,20 @@ public class NotifyMessageMapper implements MessageMapper{
 	@Override
 	public String toMessageBody(TridentTuple tuple) {
 		// TODO Auto-generated method stub
-		return null;
+		StringBuilder sb = new StringBuilder();
+		
+		sb.append("On " + new Date(tuple.getLongByField("timestamp")) + " ");
+		
+		sb.append("the application \"" + tuple.getStringByField("logger") + "\"");
+		
+		sb.append("changed alert state based on a threshold of " + tuple.getDoubleByField("threshold") + ".\n");
+		
+		sb.append("The last value was " + tuple.getDoubleByField("average") + "\n");
+		
+		sb.append("The last message was \"" + tuple.getStringByField("message") + "\"");
+		
+		return sb.toString();
+		
 	}
 
 }
